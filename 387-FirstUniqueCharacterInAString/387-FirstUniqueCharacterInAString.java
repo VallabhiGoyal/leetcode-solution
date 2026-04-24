748c1a3f6e6af80791e7f0783f08545daff75b3c
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-24 10:34:11 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.java
@@ -0,0 +1,17 @@
+// Last updated: 4/24/2026, 10:34:12 AM
+class Solution {
+    public int firstUniqChar(String s) {
+        int n = s.length();
+
+        int[] freq = new int[26];
+        for(int i = 0; i<n; i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i<n; i++){
+            if(freq[s.charAt(i) - 'a'] == 1) return i;
+        }
+
+        return -1;
+    }
+}
