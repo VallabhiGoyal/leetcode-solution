23f76ca35799ed0a9b6a59827c93e10a6d50f1fb
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-24 10:30:45 +0530

    Create 2813-ToBeOrNotToBe

---
+++ 2813-ToBeOrNotToBe/2813-ToBeOrNotToBe.js
@@ -0,0 +1,24 @@
+// Last updated: 4/24/2026, 10:30:46 AM
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function (otherVal){
+            if(val === otherVal){
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+        notToBe : function(otherVal){
+            if(val !== otherVal) return true;
+            throw new Error("Equal");
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
