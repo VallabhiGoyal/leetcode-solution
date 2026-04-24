f8273f51ff9f6b673108949240017667b8c8cd4e
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-24 10:31:16 +0530

    Create 2107-FindUniqueBinaryString

---
+++ 2107-FindUniqueBinaryString/2107-FindUniqueBinaryString.java
@@ -0,0 +1,12 @@
+// Last updated: 4/24/2026, 10:31:17 AM
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i].charAt(i) == '0') str.append("1");
+            else str.append("0");
+        }
+
+        return str.toString();
+    }
+}
