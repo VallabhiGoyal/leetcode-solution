527c5ff0a1ec17e6b08fc1989bf3ed15c381666b
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-24 10:30:50 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 4/24/2026, 10:30:52 AM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
