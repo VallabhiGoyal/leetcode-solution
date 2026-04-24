dc621c07fd34d0e5d480eff9488cd1266b25cc19
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-24 10:31:56 +0530

    Create 1444-NumberOfStepsToReduceANumberToZero

---
+++ 1444-NumberOfStepsToReduceANumberToZero/1444-NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,17 @@
+// Last updated: 4/24/2026, 10:31:57 AM
+class Solution {
+    public int numberOfSteps(int num) {
+        return helper(num, 0);
+    }
+    public int helper(int num, int steps){
+        if(num==0){
+            return steps;
+        }
+        if(num%2==0){
+            return helper(num/2, steps+1);
+        }
+            
+        return helper(num-1, steps+1);
+        
+    }
+}
