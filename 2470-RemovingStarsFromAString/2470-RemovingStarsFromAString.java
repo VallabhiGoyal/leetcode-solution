3817a740f1fa32d3e3a70e6fc3ba1528bc9fbd7c
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-24 10:30:58 +0530

    Create 2470-RemovingStarsFromAString

---
+++ 2470-RemovingStarsFromAString/2470-RemovingStarsFromAString.java
@@ -0,0 +1,18 @@
+// Last updated: 4/24/2026, 10:30:59 AM
+class Solution {
+    public String removeStars(String s) {
+        int n = s.length();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<n; i++){
+            char ch = s.charAt(i);
+            if(ch == '*'){
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+}
