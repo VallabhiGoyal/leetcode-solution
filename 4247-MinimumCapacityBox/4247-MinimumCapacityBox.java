04dc4d45c7b7350eb77de858d7f6865c50425264
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-24 10:29:33 +0530

    Create 4247-MinimumCapacityBox

---
+++ 4247-MinimumCapacityBox/4247-MinimumCapacityBox.java
@@ -0,0 +1,16 @@
+// Last updated: 4/24/2026, 10:29:34 AM
+class Solution {
+    public int minimumIndex(int[] capacity, int itemSize) {
+        int n = capacity.length;
+        int idx = -1;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i<n; i++){
+            if(capacity[i]<min && capacity[i]>=itemSize){
+                min = capacity[i];
+                idx = i;
+            }
+        }
+
+        return idx;
+    }
+}
