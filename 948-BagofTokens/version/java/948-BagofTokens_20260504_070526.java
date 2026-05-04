4007051761c081f6f14a50c2a3d8009c49437768
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-05-04 07:05:26 +0530

    Create 948-BagofTokens

---
+++ 948-BagofTokens/version/java/948-BagofTokens_20260504_070526.java
@@ -0,0 +1,36 @@
+// Last updated: 5/4/2026, 7:05:26 AM
+//T.C : O(nlogn)
+//S.C : O(1)
+public class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        int n = tokens.length;
+        Arrays.sort(tokens);
+
+        int currScore = 0;
+        int maxScore = 0;
+        int l = 0, r = n - 1;
+
+        // GREEDY
+        // While losing power, I will choose the smallest token
+        // While gaining power, I will choose the largest token
+
+        while (l <= r) {
+            if (power >= tokens[l]) {
+                currScore++;
+                maxScore = Math.max(maxScore, currScore); // keep updating it
+                power -= tokens[l]; // choose smallest token
+                l++;
+
+            } else if (currScore >= 1) {
+                currScore--;
+                power += tokens[r]; // choose largest token
+                r--;
+
+            } else {
+                // no way further to increase score
+                return maxScore;
+            }
+        }
+        return maxScore;
+    }
+}
