a131459c6c5443b64ce81c5bee1f4232b51eb1ae
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-07-04 09:10:25 +0530

    Create 1372-LongestZigZagPathinaBinaryTree

---
+++ 1372-LongestZigZagPathinaBinaryTree/version/java/1372-LongestZigZagPathinaBinaryTree_20260704_091027.java
@@ -0,0 +1,51 @@
+// Last updated: 7/4/2026, 9:10:27 AM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    Integer MAX = 0;
+18
+19    public int longestZigZag(TreeNode root) {
+20        dfs(root, false, 0);
+21        dfs(root, true, 0);
+22
+23        return MAX;
+24    }
+25
+26    public void dfs(TreeNode root, boolean isLeft, int currLen){
+27        if(root == null) return;
+28        if(root.left == null && root.right == null) return;
+29
+30        if(isLeft){
+31            if(root.right != null){
+32                dfs(root.right, false, currLen + 1);
+33                MAX = Math.max(MAX, currLen + 1);
+34            }
+35
+36            if(root.left != null){
+37                dfs(root.left, true, 1);
+38            }
+39        }else{
+40            if(root.left != null){
+41                dfs(root.left, true, currLen + 1);
+42                MAX = Math.max(MAX, currLen + 1);
+43            }
+44
+45            if(root.right != null){
+46                dfs(root.right, false, 1);
+47            }
+48        }
+49    }
+50}
