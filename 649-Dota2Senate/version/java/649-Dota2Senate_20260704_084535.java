9530d0a8576d440cf1b980a358d89b0cf1e19a2d
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-07-04 08:45:33 +0530

    Create 649-Dota2Senate

---
+++ 649-Dota2Senate/version/java/649-Dota2Senate_20260704_084535.java
@@ -0,0 +1,31 @@
+// Last updated: 7/4/2026, 8:45:35 AM
+1class Solution {
+2    public String predictPartyVictory(String senate) {
+3        int n = senate.length();
+4
+5        if(n == 1) return senate.charAt(0) == 'R' ? "Radiant" : "Dire";
+6
+7        Queue<Integer> radiant = new LinkedList<>();
+8        Queue<Integer> dire = new LinkedList<>();
+9        
+10        for(int i = 0; i<n; i++){
+11            char ch = senate.charAt(i);
+12            if(ch == 'R') radiant.add(i);
+13            else dire.add(i);
+14        }
+15
+16        while(!radiant.isEmpty() && !dire.isEmpty()){
+17            if(radiant.peek() < dire.peek()){
+18                dire.remove();
+19                int curr = radiant.poll();
+20                radiant.add(curr + n);
+21            }else{
+22                radiant.remove();
+23                int curr = dire.poll();
+24                dire.add(curr + n);
+25            }
+26        }
+27
+28        return radiant.size() > dire.size() ? "Radiant" : "Dire";
+29    }
+30}
