ef8ad6f754bec59c7362573b28cc1f08f326b915
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-07-15 06:35:03 +0530

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/version/java/56-MergeIntervals_20260715_063503.java
@@ -0,0 +1,30 @@
+// Last updated: 7/15/2026, 6:35:03 AM
+1class Solution {
+2    public int[][] merge(int[][] intervals) {
+3        Arrays.sort(intervals, (a,b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+4
+5        List<int[]> list = new ArrayList<>();
+6        
+7        int start = intervals[0][0];
+8        int end = intervals[0][1];
+9
+10        int n = intervals.length;
+11
+12        for(int i = 0; i<n; i++){
+13            int a = intervals[i][0];
+14            int b = intervals[i][1];
+15
+16            if(start <= a && end >= a){
+17                end = Math.max(end, b);
+18            }else{
+19                list.add(new int[] {start, end});
+20                start = a;
+21                end = b;
+22            }
+23        }
+24
+25        list.add(new int[]{start, end});
+26
+27        return list.toArray(new int[list.size()][]);
+28    }
+29}
