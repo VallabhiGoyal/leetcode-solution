7a6a7ea8b8f3ebc931c4c9bfe0387769971a78cd
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-07-15 06:16:50 +0530

    Create 3658-GCDofOddandEvenSums

---
+++ 3658-GCDofOddandEvenSums/version/java/3658-GCDofOddandEvenSums_20260715_061650.java
@@ -0,0 +1,9 @@
+// Last updated: 7/15/2026, 6:16:50 AM
+1class Solution {
+2    public int gcdOfOddEvenSums(int n) {
+3        long sumOdd = n*n;
+4        long sumEven = n*(n + 1);
+5
+6        return n;
+7    }
+8}
