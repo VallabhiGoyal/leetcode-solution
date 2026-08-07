55a7ec84fdcffbae3c588ea4c7e8095c8691308c
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-08-07 07:02:12 +0530

    Create 71-SimplifyPath

---
+++ 71-SimplifyPath/version/java/71-SimplifyPath_20260807_070213.java
@@ -0,0 +1,27 @@
+// Last updated: 8/7/2026, 7:02:13 AM
+1class Solution {
+2    public String simplifyPath(String path) {
+3        String[] temp = path.split("/+");
+4
+5        Stack<String> stack = new Stack<>();
+6
+7        for (String s : temp) {
+8            if (s.equals("") || s.equals(".")) {
+9                continue;
+10            } else if (s.equals("..")) {
+11                if (!stack.isEmpty()) stack.pop();
+12            } else {
+13                stack.push(s);
+14            }
+15        }
+16
+17        if (stack.isEmpty()) return "/";
+18
+19        StringBuilder sb = new StringBuilder();
+20        for (String dir : stack) {
+21            sb.append('/').append(dir);
+22        }
+23
+24        return sb.toString();
+25    }
+26}
