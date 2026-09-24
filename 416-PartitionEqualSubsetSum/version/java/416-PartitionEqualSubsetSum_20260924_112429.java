0d0f12bd008ff1649500bdf69f812a131b303ee9
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-09-24 11:24:30 +0530

    Create 416-PartitionEqualSubsetSum

---
+++ 416-PartitionEqualSubsetSum/version/java/416-PartitionEqualSubsetSum_20260924_112429.java
@@ -0,0 +1,22 @@
+// Last updated: 9/24/2026, 11:24:29 AM
+1class Solution {
+2    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+3        int n = rowSum.length;
+4        int m = colSum.length;
+5
+6        int[][] ans = new int[n][m];
+7
+8        int i = 0;
+9        int j = 0;
+10        while(i < n && j < m){
+11            ans[i][j] = Math.min(rowSum[i], colSum[j]);
+12            rowSum[i] -= ans[i][j];
+13            colSum[j] -=  ans[i][j];
+14            
+15            if(rowSum[i] == 0) i++;
+16            if(colSum[j] == 0) j++;
+17        }
+18
+19        return ans;
+20    }
+21}
